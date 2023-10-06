5c7cf1650eeb2b6b43e7b6fd074209f3ee66df4c
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2023-10-06 07:03:51 -0700

    add programmatic setup and tableViewController

---
+++ SmallProject1/SmallProject1/SmallProject1/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  SmallProject1
+//
+//  Created by Immanuel Matthews-Feemster on 10/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        setupUI()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    private func setupUI() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+}
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+}
+
